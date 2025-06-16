f3fde115b02465de437e53427096d5fa259e0401
itsPrudy <dencecira463@gmail.com>
2025-06-16 19:28:20 +0300

    built the hero section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/Hero";
 import Navbar from "./components/NavBar";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
