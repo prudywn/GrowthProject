9e6f6fa6a57809cc5695948149164499d0bab357
itsPrudy <dencecira463@gmail.com>
2025-06-24 10:33:16 +0300

    added queries tothearticlespage and about page

---
+++ frontend/src/lib/fetcher.ts
@@ -0,0 +1,52 @@
+import { sanityClient } from "@/lib/sanity";
+import { getAllPostsQuery, getHomepageContentQuery } from "./queries";
+import {
+  getAboutPageContentQuery,
+  getAllCoreValuesQuery,
+  getAllCourseLessonsQuery,
+  getAllTeamMembersQuery,
+  getAllTestimonialsQuery,
+  getAllTrustedClientsQuery,
+  getWhyUsContentQuery,
+  getAllWhyUsPointsQuery
+} from "./queries";
+
+export const fetchHomepageContent = async () => {
+  return await sanityClient.fetch(getHomepageContentQuery);
+};
+
+export const fetchArticles = async () => {
+  return await sanityClient.fetch(getAllPostsQuery);
+};
+
+export const fetchAboutPageContent = async () => {
+  return await sanityClient.fetch(getAboutPageContentQuery);
+};
+
+export const fetchCoreValues = async () => {
+  return await sanityClient.fetch(getAllCoreValuesQuery);
+};
+
+export const fetchCourseLessons = async () => {
+  return await sanityClient.fetch(getAllCourseLessonsQuery);
+};
+
+export const fetchTeamMembers = async () => {
+  return await sanityClient.fetch(getAllTeamMembersQuery);
+};
+
+export const fetchTestimonials = async () => {
+  return await sanityClient.fetch(getAllTestimonialsQuery);
+};
+
+export const fetchTrustedClients = async () => {
+  return await sanityClient.fetch(getAllTrustedClientsQuery);
+};
+
+export const fetchWhyUsContent = async () => {
+  return await sanityClient.fetch(getWhyUsContentQuery);
+};
+
+export const fetchWhyUsPoints = async () => {
+  return await sanityClient.fetch(getAllWhyUsPointsQuery);
+};
--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -62,3 +62,108 @@ export const getRelatedPostsQuery = `
     body
   }
 `;
+
+export const getHomepageContentQuery = `
+  *[_type == "homepageContent"][0]{
+    heroTitle,
+    heroDescription,
+    clientsCount,
+    professionalsTrainedCount,
+    yearsOfExperience,
+    peopleRecruitedCount
+  }
+`;
+
+// About Page Content
+export const getAboutPageContentQuery = `
+  *[_type == "aboutPageContent"][0]{
+    missionStatement,
+    missionImage { asset->{ url } },
+    valuesStatement,
+    coreValues[]->{
+      _id,
+      name,
+      text,
+      iconKey
+    }
+  }
+`;
+
+// Core Values
+export const getAllCoreValuesQuery = `
+  *[_type == "coreValue"]{
+    _id,
+    name,
+    text,
+    image { asset->{ url } }
+  }
+`;
+
+// Course Lessons
+export const getAllCourseLessonsQuery = `
+  *[_type == "courseLesson"] | order(lessonNumber asc) {
+    _id,
+    lessonName,
+    lessonDescription,
+    lessonVideoUrl,
+    lessonImage { asset->{ url } },
+    lessonTranscript,
+    lessonNumber
+  }
+`;
+
+// Team Members
+export const getAllTeamMembersQuery = `
+  *[_type == "teamMember"]{
+    _id,
+    name,
+    role,
+    image { asset->{ url } }
+  }
+`;
+
+// Testimonials
+export const getAllTestimonialsQuery = `
+  *[_type == "testimonial"]{
+    _id,
+    personName,
+    personRole,
+    clientCompanyName,
+    responseText
+  }
+`;
+
+// Trusted Clients
+export const getAllTrustedClientsQuery = `
+  *[_type == "trustedClient"]{
+    _id,
+    name,
+    logo { asset->{ url } }
+  }
+`;
+
+// Why Us Content
+export const getWhyUsContentQuery = `
+  *[_type == "whyUsContent"][0]{
+    sectionDescription,
+    miniboxTitle,
+    miniboxDescription,
+    miniboxImage { asset->{ url } },
+    whyUsPoints[]->{
+      _id,
+      title,
+      reason,
+      image { asset->{ url } }
+    }
+  }
+`;
+
+// Why Us Points
+export const getAllWhyUsPointsQuery = `
+  *[_type == "whyUsPoint"]{
+    _id,
+    title,
+    reason,
+    image { asset->{ url } }
+  }
+`;
